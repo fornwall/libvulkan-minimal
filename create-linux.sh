d432c0833d056ac0f8b0d574d22417a704ca98ec
Fredrik Fornwall <fredrik@fornwall.net>
2023-11-18 21:48:34 +0100

    Initial script

--- create-linux.sh
+++ create-linux.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 set -e -u
 
-VULKAN_SDK_VERSION=1.3.268.0
-VULKAN_SDK_URL=https://sdk.lunarg.com/sdk/download/${VULKAN_SDK_VERSION}/linux/vulkansdk-linux-x86_64-${VULKAN_SDK_VERSION}.tar.xz
+# VULKAN_SDK_VERSION=1.3.268.0
+VULKAN_SDK_TARNAME=vulkansdk-linux-x86_64-${VULKAN_SDK_VERSION}.tar.xz
+VULKAN_SDK_MINIMAL_TARNAME=vulkansdk-linux-minimal-x86_64-${VULKAN_SDK_VERSION}.tar.xz
+VULKAN_SDK_URL=https://sdk.lunarg.com/sdk/download/${VULKAN_SDK_VERSION}/linux/$VULKAN_SDK_TARNAME
 
 curl -O $VULKAN_SDK_URL
+
+tar xf $VULKAN_SDK_TARNAME
+
+mv $VULKAN_SDK_VERSION $VULKAN_SDK_VERSION-full
+
+mkdir -p $VULKAN_SDK_VERSION/x86_64/{include/vulkan,lib}
+
+cp -Rf $VULKAN_SDK_VERSION-full/x86_64/include/vulkan/* $VULKAN_SDK_VERSION/x86_64/include/vulkan/
+
+cp $VULKAN_SDK_VERSION-full/x86_64/lib/{libvulkan*,libVkLayer_khronos_validation.so} $VULKAN_SDK_VERSION/x86_64/lib/
+
+# Shrink from around 600 MB to 40:
+strip $VULKAN_SDK_VERSION/x86_64/lib/libVkLayer_khronos_validation.so
+
+tar cf $VULKAN_SDK_MINIMAL_TARNAME $VULKAN_SDK_VERSION
